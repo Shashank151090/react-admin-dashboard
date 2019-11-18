c0e6a870939afb224606903840054a101cb22190
Shashank <shashank151090@gmail.com>
2019-11-19 00:07:47 +0530

    init commit

---
+++ src/routes.js
@@ -0,0 +1,38 @@
+
+import Dashboard from "views/Dashboard.jsx";
+import Icons from "views/Icons.jsx";
+import Map from "views/Map.jsx";
+import Notifications from "views/Notifications.jsx";
+import Rtl from "views/Rtl.jsx";
+import TableList from "views/TableList.jsx";
+import Typography from "views/Typography.jsx";
+import UserProfile from "views/UserProfile.jsx";
+
+var routes = [
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    rtlName: "لوحة القيادة",
+    icon: "tim-icons icon-chart-pie-36",
+    component: Dashboard,
+    layout: "/admin"
+  },
+
+  {
+    path: "/user-profile",
+    name: "User Profile",
+    rtlName: "ملف تعريفي للمستخدم",
+    icon: "tim-icons icon-single-02",
+    component: UserProfile,
+    layout: "/admin"
+  },
+  {
+    path: "/tables",
+    name: "Table List",
+    rtlName: "قائمة الجدول",
+    icon: "tim-icons icon-puzzle-10",
+    component: TableList,
+    layout: "/admin"
+  }
+];
+export default routes;
